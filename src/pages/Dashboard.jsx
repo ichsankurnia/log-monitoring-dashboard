4be3df96e75ce228e25075da9c1d76be5486ad1a
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-03-20 22:23:02 +0700

    add routes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,19 +1,21 @@
 import { Breadcrumb, Layout } from 'antd';
 import Avatar from 'antd/lib/avatar/avatar';
 import Title from 'antd/lib/typography/Title';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Sidebar from '../components/Sidebar';
+import routes from '../routes';
 
 const { Header, Footer, Sider, Content } = Layout;
 
 const Dashboard = () => {
 
     return (
-        <div>
+        <BrowserRouter>
             <Layout style={{height: '100vh', overflow: 'hidden'}}>
             {/* <Layout> */}
                 <Header style={{padding: 10}}>
                     <Avatar style={{float: 'right'}} icon="user" />
-                    <Title style={{color: 'white'}} level={3}>Log Apps Monitoring</Title>
+                    <Title style={{color: 'white'}} level={3}>Log Monitoring Apps</Title>
                 </Header>
                 <Layout>
                     <Sider>
@@ -25,13 +27,24 @@ const Dashboard = () => {
                                 <Breadcrumb.Item>Home</Breadcrumb.Item>
                                 <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
                             </Breadcrumb>
-                            <div style={{background: '#fff', padding: 24, minHeight: '75vh'}}>Content</div>
+                            <div style={{background: '#fff', padding: 24, minHeight: '75vh'}}>
+                                <Switch>
+                                    {routes.map((route, index) => (
+                                        <Route 
+                                        key={index}
+                                        path={route.path}
+                                        exact={route.exact}
+                                        component={route.component}
+                                        />
+                                        ))}
+                                </Switch>
+                            </div>
                         </Content>
                         <Footer style={{textAlign: 'center'}}>Ant Design ©2021 Created by ichsankurnia</Footer>
                     </Layout>
                 </Layout>
             </Layout>
-        </div>
+        </BrowserRouter>
     )
 }
 
