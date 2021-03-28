7ca22c08670cd7ea9641a7ab1e2c57ab24d74edb
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-03-28 23:37:00 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-// import Login from "./pages/Login";
+import Login from "./pages/Login";
+import NotFoundPage from "./pages/NotFoundPage";
 
-function App() {
+function App(props) {
 	return (
-		<>
-			<Dashboard />
+		<BrowserRouter>
+			<Switch>
+				{/* <Route path="/auth" render={props => <Login {...props} />} />
+				<Route path="/" exact render={props => <Dashboard {...props} />} />
+				<Route path="*" render={props => <NotFoundPage {...props} />} /> */}
+				<Route path="/auth" component={Login} />
+				<Route path="/" exact component={Dashboard} />
+				<Route path="*" component={NotFoundPage} />
+			</Switch>
 			{/* <Login /> */}
-		</>
+		</BrowserRouter>
 	);
 }
 
