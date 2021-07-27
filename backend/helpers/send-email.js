25299b9f492383e569074976433c85d4e32989b6
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-07-27 15:44:56 +0700

    fixing default email auth

--- backend/helpers/send-email.js
+++ backend/helpers/send-email.js
@@ -3,16 +3,16 @@ require('dotenv').config()
 var nodemailer = require('nodemailer');
 
 
-const { GMAIL_USER, GMAIL_PASS, MAIL_RECIPIENTS } = process.env
+const { MAIL_HOST, MAIL_USER, MAIL_PASS, MAIL_RECIPIENTS } = process.env
 
 const config = {
-    service: 'gmail',
-    host: 'smtp.gmail.com',
+    // service: 'gmail',
+    host: MAIL_HOST || 'goes2nobel.com',
     port: 465,  // 587
     secure: true, // true for 465, false for other ports
     auth: {
-        user: GMAIL_USER,
-        pass: GMAIL_PASS
+        user: MAIL_USER || 'info@goes2nobel.com',
+        pass: MAIL_PASS || 'goestonobel123'
     }
 }
 
@@ -20,7 +20,7 @@ const sendEmailFormAduan = async (desc, xlsBase64) => {
     var transporter = await nodemailer.createTransport(config);
 
     var mailOptions = {
-        from: `"Operasional NI" ${GMAIL_USER}`,
+        from: `"Operasional NI" ${MAIL_USER || 'info@goes2nobel.com'}`,
         to: MAIL_RECIPIENTS.split(','),
         subject: `Permintaan / Perbaikan Barang ${desc}`,
         text: "Berikut Terlampir Detail Permintaan", // plain text body
