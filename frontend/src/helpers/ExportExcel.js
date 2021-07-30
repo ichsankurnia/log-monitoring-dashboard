791a1d501b18f2c9d0e02f80942912564b806f3c
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-07-30 19:53:53 +0700

    fixing export excel documentation

--- frontend/src/helpers/ExportExcel.js
+++ frontend/src/helpers/ExportExcel.js
@@ -149,157 +149,165 @@ class ExportExcel {
     }
 
     static exportDocumentation = async (srcDataTable) => {
-        const columns = [
-            { header: 'No', key: 'number', width: 5, style: {numFmt: '0;[Red]0'}},
-            { header: 'No Ticket', key: 'no', width: 25 },
-            { header: 'Tanggal Masalah', key: 'tanggal_masalah', style: {numFmt: 'dd/mm/yyyy'} },
-            { header: 'Jam Masalah', key: 'jam_masalah', style: {numFmt: '[$-13809]hh:mm:ss;@'} },
-            { header: 'Tanggal Done', key: 'tanggal_done', style: {numFmt: 'dd/mm/yyyy'} },
-            { header: 'Jam Done', key: 'jam_done', style: {numFmt: '[$-13809]hh:mm:ss;@'} },
-            { header: 'Jenis Laporan', key: 'jenislaporan' },
-            { header: 'Projek', key: 'nama_projek' },
-            { header: 'Lokasi', key: 'nama_stasiun' },
-            { header: 'Perangkat', key: 'nama_perangkat' },
-            { header: 'Part', key: 'nama_part' },
-            { header: 'Masalah', key: 'problem' },
-            { header: 'Penyebab', key: 'penyebab' },
-            { header: 'Solusi', key: 'solusi' },
-            { header: 'Sumber', key: 'sumber' },
-            { header: 'RefNoTrouble', key: 'refnotrouble' },
-            { header: 'Teknisi', key: 'teknisi' },
-            { header: 'Downtime', key: 'totaldowntime' },
-            { header: 'Status', key: 'status' },
-            { header: 'Picture Before', key: 'pic_before', width: 25 },
-            { header: 'Picture After', key: 'pic_after', width: 25 },
-        ];
-
-        const workbook = new ExcelJS.Workbook();
-        workbook.creator = 'ichsankurnia';
-        workbook.lastModifiedBy = 'Ories';
-
-        var worksheet = workbook.addWorksheet("Sheet1", {orientation:'landscape'});
-
-        const base64Img = await Helper.toDataURL(logoNtcNT)
-        var logo = workbook.addImage({
-            base64: base64Img,
-            extension: 'jpeg',
-        });
-        worksheet.addImage(logo, {
-            tl: { col: 1, row: 1 },
-            ext: { width: 250, height: 75 }
-        })
-        
-        worksheet.mergeCells('B6:I6')
-        const titleCell = worksheet.getCell('B6')
-        titleCell.font = {
-            size: 14,
-            underline: true,
-            bold: true
-        }
-        titleCell.alignment = {
-            horizontal: 'center'
-        }
-        titleCell.value = "Export TroubleET"
-
-
-        // Table Headers
-        worksheet.getRow(8).values = columns.map(item => item.header);
-        worksheet.getRow(8).font = { bold: true }
-        worksheet.getRow(8).eachCell({ includeEmpty: false }, cell => {
-            cell.border = border
-        })
-        worksheet.columns = columns.map(item =>{ 
-            return {
-                key: item.key, 
-                width: item.width || 20,
-                style: item.style || { numFmt: 'General' }
-            }
-        })
-
-        // Set table value
-        srcDataTable.forEach((item, index) => {
-            const row = worksheet.addRow({
-                number: index + 1,
-                no: item.no,
-                tanggal_masalah: new Date(item.tanggal_masalah).toLocaleDateString("id-ID", {timeZone: "Asia/Jakarta"}),
-                jam_masalah: item.jam_masalah,
-                tanggal_done: new Date(item.tanggal_done).toLocaleDateString("id-ID", {timeZone: "Asia/Jakarta"}),
-                jam_done: item.jam_done,
-                jenislaporan: item.jenislaporan,
-                nama_projek: item.nama_projek,
-                nama_stasiun: item.nama_stasiun,
-                nama_perangkat: item.nama_perangkat,
-                nama_part: item.nama_part,
-                problem: item.problem,
-                penyebab: item.penyebab,
-                solusi: item.solusi,
-                sumber: item.sumber,
-                refnotrouble: item.refnotrouble,
-                teknisi: item.teknisi,
-                totaldowntime: item.totaldowntime,
-                status: item.status
+        try {
+            const columns = [
+                { header: 'No', key: 'number', width: 5, style: {numFmt: '0;[Red]0'}},
+                { header: 'No Ticket', key: 'no', width: 25 },
+                { header: 'Tanggal Masalah', key: 'tanggal_masalah', style: {numFmt: 'dd/mm/yyyy'} },
+                { header: 'Tanggal Done', key: 'tanggal_done', style: {numFmt: 'dd/mm/yyyy'} },
+                { header: 'Projek', key: 'nama_projek' },
+                { header: 'Lokasi', key: 'nama_stasiun' },
+                { header: 'Perangkat', key: 'nama_perangkat' },
+                { header: 'ID Perangkat', key: 'id_perangkat' },
+                { header: 'Jenis Pergantian', key: 'nama_part' },
+                { header: 'Problem', key: 'problem' },
+                { header: 'Solusi', key: 'solusi' },
+                { header: 'Picture Before', key: 'pic_before', width: 25 },
+                { header: 'Picture After', key: 'pic_after', width: 25 },
+            ];
+    
+            const workbook = new ExcelJS.Workbook();
+            workbook.creator = 'ichsankurnia';
+            workbook.lastModifiedBy = 'Ories';
+    
+            var worksheet = workbook.addWorksheet("Dokumentasi Pemasangan Barang", {orientation:'landscape'});
+    
+            const base64Img = await Helper.toDataURL(logoNtcNT)
+            var logo = workbook.addImage({
+                base64: base64Img,
+                extension: 'jpeg',
+            });
+            worksheet.addImage(logo, {
+                tl: { col: 1, row: 1 },
+                ext: { width: 250, height: 75 }
             })
-
-            // console.log(row)
-            row.height = 70                                                                                                         // SET ROW HEIGHT
-
-            row._cells.forEach(cell => {
+            
+            worksheet.mergeCells('A6:M6')
+            const titleCell = worksheet.getCell('B6')
+            titleCell.font = {
+                size: 14,
+                underline: true,
+                bold: true
+            }
+            titleCell.alignment = {
+                horizontal: 'center'
+            }
+            titleCell.value = "Dokumentasi Pemasangan Barang"
+    
+    
+            // Table Headers
+            worksheet.getRow(8).values = columns.map(item => item.header);
+            worksheet.getRow(8).font = { bold: true }
+            worksheet.getRow(8).eachCell({ includeEmpty: false }, cell => {
                 cell.border = border
-                cell.alignment = {
-                    vertical: 'middle',
-                    wrapText: true
+            })
+            worksheet.columns = columns.map(item =>{ 
+                return {
+                    key: item.key, 
+                    width: item.width || 20,
+                    style: item.style || { numFmt: 'General' }
                 }
-                // console.log(cell)
             })
+    
+            // Set table value
+            srcDataTable.forEach((item, index) => {
+                const row = worksheet.addRow({
+                    number: index + 1,
+                    no: item.no,
+                    tanggal_masalah: new Date(item.tanggal_masalah).toLocaleDateString("id-ID", {timeZone: "Asia/Jakarta"}),
+                    tanggal_done: new Date(item.tanggal_done).toLocaleDateString("id-ID", {timeZone: "Asia/Jakarta"}),
+                    nama_projek: item.nama_projek,
+                    nama_stasiun: item.nama_stasiun,
+                    nama_perangkat: item.nama_perangkat,
+                    id_perangkat: item.id_perangkat,
+                    nama_part: item.nama_part,
+                    problem: item.problem,
+                    solusi: item.solusi,
+                })
+    
+                // console.log(row)
+                row.height = 70                                                                                                         // SET ROW HEIGHT
+    
+                row._cells.forEach(cell => {
+                    cell.border = border
+                    cell.alignment = {
+                        vertical: 'middle',
+                        wrapText: true
+                    }
+                    // console.log(cell)
+                })
+    
+                const picBefore = Helper.getASCIIAsBase64(item.pic_before)
+                const picAfter = Helper.getASCIIAsBase64(item.pic_after)
+                if(picBefore && picBefore.includes('base64')){
+                    worksheet.addImage(workbook.addImage({base64: picBefore, extension: 'jpeg'}), {
+                        // tl: { col: columns.length - 2 + 0.2, row: row._number - 1 + 0.1 }, br: { col: columns.length - 1 - 0.2, row: row._number - 0.1 }, editAs: 'absolute'
+                        tl: { col: columns.length - 2 + 0.1, row: row._number - 1 + 0.2 }, ext: { width: 160, height: 80 }
+                    })
+                }else{
+                    const cell = `${columnToLetter(columns.length - 1)}${row._number}`
+                    worksheet.getCell(cell).value = 'File too large, cannot load the image!'
+                    worksheet.getCell(cell).font = {size: 7.5, color: {argb: "00FF0000"}}
+                    worksheet.getCell(cell).alignment = {vertical: 'middle', horizontal: 'center'}
+                }
 
-            worksheet.addImage(workbook.addImage({base64: base64Img, extension: 'jpeg'}), {
-                // tl: { col: columns.length - 2 + 0.2, row: row._number - 1 + 0.1 }, br: { col: columns.length - 1 - 0.2, row: row._number - 0.1 }, editAs: 'absolute'
-                tl: { col: columns.length - 2 + 0.1, row: row._number - 1 + 0.2 }, ext: { width: 160, height: 80 }
+                if(picAfter && picAfter.includes('base64')){
+                    worksheet.addImage(workbook.addImage({base64: picAfter, extension: 'jpeg'}), {
+                        // tl: { col: columns.length - 1 + 0.2, row: row._number - 1 + 0.1 }, br: { col: columns.length - 0.2, row: row._number - 0.1 }, editAs: 'oneCell'
+                        tl: { col: columns.length - 1 + 0.1, row: row._number - 1 + 0.2 }, ext: { width: 160, height: 80 }
+                    })
+                }else{
+                    const cell = `${columnToLetter(columns.length)}${row._number}`
+                    worksheet.getCell(cell).value = 'File too large, cannot load the image!'
+                    worksheet.getCell(cell).font = {size: 7.5, color: {argb: "00FF0000"}}
+                    worksheet.getCell(cell).alignment = {vertical: 'middle', horizontal: 'center'}
+                }
             })
-            worksheet.addImage(workbook.addImage({base64: base64Img, extension: 'jpeg'}), {
-                // tl: { col: columns.length - 1 + 0.2, row: row._number - 1 + 0.1 }, br: { col: columns.length - 0.2, row: row._number - 0.1 }, editAs: 'oneCell'
-                tl: { col: columns.length - 1 + 0.1, row: row._number - 1 + 0.2 }, ext: { width: 160, height: 80 }
+    
+            worksheet.getColumn('pic_before').eachCell({ includeEmpty: false }, cell => {
+                if(cell._row._number >= 8){
+                    cell.border = border
+                }
             })
-        })
-
-        worksheet.getColumn('pic_before').eachCell({ includeEmpty: false }, cell => {
-            if(cell._row._number >= 8){
-                cell.border = border
+            worksheet.getColumn('pic_after').eachCell({ includeEmpty: false }, cell => {
+                if(cell._row._number >= 8){
+                    cell.border = border
+                }
+            })
+    
+    
+            // Signature
+            let lastRowTable
+            await worksheet.eachRow({ includeEmpty: false }, async function(row, rowNumber) {
+                lastRowTable = await rowNumber
+            });
+            
+            const lastCellTable = `${columnToLetter(columns.length - 1)}${lastRowTable + 2}`
+            const signCell = `${columnToLetter(columns.length)}${lastRowTable + 4}`
+            worksheet.mergeCells(signCell, lastCellTable)
+            
+            const signCellValue = worksheet.getCell(signCell)
+            signCellValue.font = signFont
+            signCellValue.alignment = {
+                horizontal: 'center',
+                wrapText: true
             }
-        })
-        worksheet.getColumn('pic_after').eachCell({ includeEmpty: false }, cell => {
-            if(cell._row._number >= 8){
-                cell.border = border
-            }
-        })
+            signCellValue.border = border
+            signCellValue.value = "Export by:\nIchsan Kurniawan"
+    
+    
+            workbook.xlsx.writeBuffer().then(function(buffer) {
+                saveAs(
+                    new Blob([buffer], { type: "application/octet-stream" }),
+                    `Dokumentasi Pemasangan Barang.xlsx`
+                );
+            });
 
-
-        // Signature
-        let lastRowTable
-        await worksheet.eachRow({ includeEmpty: false }, async function(row, rowNumber) {
-            lastRowTable = await rowNumber
-        });
-        
-        const lastCellTable = `${columnToLetter(columns.length - 1)}${lastRowTable + 2}`
-        const signCell = `${columnToLetter(columns.length)}${lastRowTable + 4}`
-        worksheet.mergeCells(signCell, lastCellTable)
-        
-        const signCellValue = worksheet.getCell(signCell)
-        signCellValue.font = signFont
-        signCellValue.alignment = {
-            horizontal: 'center',
-            wrapText: true
+            return true
+        } catch (error) {
+            console.log(error)
+            return false
         }
-        signCellValue.border = border
-        signCellValue.value = "Export by:\nIchsan Kurniawan"
-
-
-        workbook.xlsx.writeBuffer().then(function(buffer) {
-            saveAs(
-                new Blob([buffer], { type: "application/octet-stream" }),
-                `Dokumentasi Pemasangan Barang.xlsx`
-            );
-        });
     }
 
     static exportFormAduan = async (user, data) => {
