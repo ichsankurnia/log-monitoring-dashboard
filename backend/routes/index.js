14c0c06d4d3b6ad313f0a9e6e4c6793f258b52dc
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-08-01 20:58:05 +0700

    add route for auth (login logout)

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,28 @@
 const {Router} = require("express")
+
 const { TroubleETController } = require("../controllers/troubleET-controller")
 const { getAllUser, getOneUser, addNewUser, editUser, deleteUser } = require("../controllers/user-controller")
+const login = require("../middlewares/authenticate")
+const Login = require("../models/login-model")
 
 const router = Router()
 
 router.get('/', (req,res) => {
     return res.json({code: 0, message: 'success', description: 'endPoint api log apps monitoring'})
+})
+
+router.post('/auth/login', login)
+router.post('/auth/logout', async (req, res) => {
+    try {
+        const data = await Login.update(req.body)
+        if(data){
+            return res.json({code: 0, message: 'success logout', data})
+        }else{
+            return res.json({code: 1, message: 'failed to logout', data: null})
+        }
+    } catch (error) {
+        return res.json({code: 1, message: error.message, data: null})
+    }
 })
 
 router.get('/user', getAllUser)
