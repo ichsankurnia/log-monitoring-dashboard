f9fdc5487eb894e92f7495129c06902da301094c
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-08-02 15:21:23 +0700

    integrated redux to app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 // import './index.css'
 import App from './App';
+import { Provider } from 'react-redux';
+import configureStore from './redux/store/configureStore';
+
+const store = configureStore()
 
 ReactDOM.render(
-  <App />,
+	<Provider store={store}>
+		<App />
+	</Provider>,
   document.getElementById('root')
 );
