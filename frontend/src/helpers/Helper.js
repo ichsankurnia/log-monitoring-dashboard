965965a0c3e12c136bd94db2c4225ba5d9dfb083
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-08-04 23:44:12 +0700

    add filter table function

--- frontend/src/helpers/Helper.js
+++ frontend/src/helpers/Helper.js
@@ -1,3 +1,5 @@
+import moment from "moment"
+
 class Helper {
     static toDataURL = (url) => fetch(url)
         .then(response => response.blob())
@@ -9,6 +11,11 @@ class Helper {
         }
     ))
 
+    /**
+     * 
+     * @param {Buffer} buffer 
+     * @returns 
+     */
     static getASCIIAsBase64 = (buffer) => {
         try {
             return new Buffer.from(buffer).toString('ascii')
@@ -17,6 +24,11 @@ class Helper {
         }
     }
 
+    /**
+     * 
+     * @param {String} letter 
+     * @returns 
+     */
     static capitalEachWord = (letter) => {
         var separateWord = letter.toLowerCase().split(' ');
         for (var i = 0; i < separateWord.length; i++) {
@@ -24,7 +36,50 @@ class Helper {
            separateWord[i].substring(1);
         }
         return separateWord.join(' ');
-     }
+    }
+
+    /**
+     * 
+     * @param {Object} objFilter 
+     * @param {Array<Object>} oriData 
+     * @returns
+     */
+    static filterDataTable = (objFilter, oriData) => {
+        try {
+            let dataTable = [...oriData]
+            let result = []
+    
+            if(Object.keys(objFilter).length > 0){
+                let x = 0
+                Object.keys(objFilter).forEach(key => {
+                    x += 1
+                    if(x > 1){
+                        if(key === "tanggal_done"){
+                            result = result.filter(data =>
+                                moment(data[key], moment(data[key]).creationData().format).isBetween(objFilter[key][0], objFilter[key][1])
+                            )
+                        }else{
+                            result = result.filter(data => data[key] === objFilter[key])
+                        }
+                    }else{
+                        if(key === "tanggal_done"){
+                            result = dataTable.filter(data =>
+                                moment(data[key], moment(data[key]).creationData().format).isBetween(objFilter[key][0], objFilter[key][1])
+                            )
+                        }else{
+                            result = dataTable.filter(data => data[key] === objFilter[key])
+                        }
+                    }
+                })
+            }else{
+                result = oriData
+            }
+    
+            return result
+        } catch (error) {
+            return oriData
+        }
+    }
 }
 
 export default Helper
