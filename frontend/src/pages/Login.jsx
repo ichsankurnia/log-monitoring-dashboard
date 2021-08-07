096d64a7b4ee6a554c27e9f1d5702414cddf353a
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-08-07 22:00:03 +0700

    hit walpaper api  every 10s

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,17 +1,61 @@
-import { Form, Input, Button, Spin } from 'antd';
-import Layout from 'antd/lib/layout/layout';
+import { useState, useEffect, useRef } from 'react';
 import { useHistory } from 'react-router';
+import axios from 'axios';
+import { Form, Input, Button, Spin } from 'antd';
+import {LoadingOutlined} from '@ant-design/icons';
 // import { Link } from 'react-router-dom';
 import { authLogin } from '../api';
-import {LoadingOutlined} from '@ant-design/icons';
-import { useState } from 'react';
 
 const loader = <LoadingOutlined style={{ fontSize: 32 }} spin />;
 
+const apiKey = '22810263-e572b6b0ebfab66c30ba7d497'
+const queryStrImg = 'image_type=photo&orientation=horizontal&category=backgrounds,nature,science,education,health,places,animals,industry,computer,food,sports,transportation,travel,buildings,business,music&pretty=true'
+
 const Login = () => {
     const [loading, setLoading] = useState(false)
+    const [count, setCount] = useState(10)
+    const [imgSource, setImgSource] = useState(null)
+    
+    const interval = useRef(null)
     const history = useHistory()
     
+    useEffect(() => {
+        fetchWallpaper()
+    }, [])
+
+    useEffect(() => {
+        interval.current = setInterval(() => {
+            if(count > 0){
+                setCount(count - 1)
+            }else{
+                fetchWallpaper()
+                setCount(10)
+            }
+        }, 1000)
+
+        return () => {
+            clearInterval(interval.current)
+        }
+
+    }, [count])
+
+    // console.log(count)
+    const fetchWallpaper = () => {
+        const page = Math.floor(Math.random() * 100) + 1
+        const index = Math.floor(Math.random() * 5)
+
+        const order = ["popular", "latest"]
+        const indexOrder = Math.floor(Math.random() * 2)
+
+        axios.get(`https://pixabay.com/api/?key=${apiKey}&${queryStrImg}&order=${order[indexOrder]}&page=${page}&per_page=5`)
+        .then((res) => {
+            console.log(res)
+            setImgSource(res.data.hits[index])
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
+
     const onFinish = async (values) => {
         setLoading(true)
         console.log('Success:', values);
@@ -36,18 +80,20 @@ const Login = () => {
     
     return (
         <Spin spinning={loading} indicator={loader} size='large'>
-            <Layout style={{height: '100vh'}} >
-                <Form style={{width: '350px', margin: 'auto'}} layout="vertical" requiredMark={false}
+            <div className='background' >
+                <img className='img-background' src={imgSource?.largeImageURL} alt="" />
+                <p className='img-tags'>{imgSource?.tags}</p>
+                <Form style={{width: '350px'}} layout="vertical" requiredMark={false}
                     name="basic" initialValues={{ remember: true}} onFinish={onFinish} onFinishFailed={onFinishFailed}>
-                    <Form.Item label={<label style={{ color: "#555", fontWeight: 500, fontSize: 16 }}>Username</label>} name="username"
+                    <Form.Item label={<label style={{ color: "#fff", fontWeight: 500, fontSize: 16 }}>Username</label>} name="username"
                         rules={[ { required: true, message: 'Please input your username!' }]}
                     >
-                        <Input />
+                        <Input placeholder='Enter your username' />
                     </Form.Item>
-                    <Form.Item label={<label style={{ color: "#555", fontWeight: 500, fontSize: 16 }}>Password</label>} name="password"
+                    <Form.Item label={<label style={{ color: "#fff", fontWeight: 500, fontSize: 16 }}>Password</label>} name="password"
                         rules={[{ required: true, message: 'Please input your password!'}]}
                     >
-                        <Input.Password />
+                        <Input.Password placeholder='Enter your password' />
                     </Form.Item>
                     <Form.Item>
                         <Button type="primary" htmlType="submit" style={{width: '100%', height: 40, marginTop: 10, borderRadius: 5}}>
@@ -56,7 +102,7 @@ const Login = () => {
                         </Button>
                     </Form.Item>
                 </Form>
-            </Layout>
+            </div>
         </Spin>
     );
 }
