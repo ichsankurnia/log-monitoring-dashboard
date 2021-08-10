55c342575807d0e7aa1869a9517fcb6278fabb0a
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-08-10 17:20:36 +0700

    disable menu admin if user login as backend

--- frontend/src/components/admin/AdminMenu.jsx
+++ frontend/src/components/admin/AdminMenu.jsx
@@ -1,11 +1,13 @@
 import { Link, useHistory } from "react-router-dom"
 import jwtDecode from "jwt-decode"
-import { AimOutlined, BugFilled, CheckCircleFilled, ExclamationCircleOutlined, FundProjectionScreenOutlined, GoldFilled, LogoutOutlined, SettingFilled, UsergroupAddOutlined } from "@ant-design/icons"
+import { BugFilled, CheckCircleFilled, EnvironmentFilled, ExclamationCircleOutlined, FundProjectionScreenOutlined, GoldFilled, LogoutOutlined, SettingFilled, UsergroupAddOutlined } from "@ant-design/icons"
 
 import { authLogout } from "../../api"
 import { Modal } from 'antd';
+import { connect } from "react-redux";
 
-const AdminMenu = () => {
+const AdminMenu = ({user}) => {
+    const admin = user?.status?.toLowerCase() === 'admin'
     const history = useHistory()
 
     const handleLogout = async () => {
@@ -25,7 +27,7 @@ const AdminMenu = () => {
         Modal.confirm({
             title: 'Logout',
             icon: <ExclamationCircleOutlined />,
-            content: 'Are sure want to leave ?',
+            content: 'Are you sure want to leave?',
             okText: 'Yes',
             cancelText: 'No',
             onOk: handleLogout
@@ -36,30 +38,42 @@ const AdminMenu = () => {
         <div className='admin-menu'>
             <h1 style={{color: 'white'}}>Administrator Menu</h1>
             <div className='admin-menu-container'>
+                {admin &&
                 <Link to='/admin/data-project' className="sub-admin-menu">
                     <FundProjectionScreenOutlined className='ic-admin-menu' />
                     <label>Project</label>
                 </Link>
+                }
+                {admin &&
                 <Link to='/admin/data-lokasi' className="sub-admin-menu">
-                    <AimOutlined className='ic-admin-menu' />
+                    <EnvironmentFilled className='ic-admin-menu' />
                     <label>Location/Station</label>
-                </Link> 
+                </Link>
+                }
+                {admin &&
                 <Link to='/admin/data-perangkat' className="sub-admin-menu">
                     <SettingFilled className='ic-admin-menu' />
                     <label>Device</label>
                 </Link> 
+                }
+                {admin &&
                 <Link to='/admin/data-part' className="sub-admin-menu">
                     <GoldFilled className='ic-admin-menu' />
                     <label>Data Part</label>
                 </Link>   
+                }
+                {admin &&
                 <Link to='/admin/data-penyebab' className="sub-admin-menu">
                     <BugFilled className='ic-admin-menu' />
                     <label>Cause</label>
                 </Link>   
+                }
+                {admin &&
                 <Link to='/admin/data-solusi' className="sub-admin-menu">
                     <CheckCircleFilled className='ic-admin-menu' />
                     <label>Solution</label>
                 </Link>  
+                }
                 <Link to='/admin/data-user' className="sub-admin-menu">
                     <UsergroupAddOutlined className='ic-admin-menu' />
                     <label>Data User</label>
@@ -76,4 +90,10 @@ const AdminMenu = () => {
     )
 }
 
-export default AdminMenu
+const mapStateToProps = (state) => {
+    return {
+        user: state.user
+    }
+}
+
+export default connect(mapStateToProps, null)(AdminMenu)
