28c5c41b63a276bd24c6692fe8531f96c6351054
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-09-13 13:36:43 +0700

    update image width

--- frontend/src/components/HomeDashboard.jsx
+++ frontend/src/components/HomeDashboard.jsx
@@ -1,11 +1,10 @@
-import logoNutechNoTrans from '../assets/img/logo-nutech-no-trans.jpg'
 import logoNutech from '../assets/img/logo-nutech.png'
 
 const HomeDashboard = () => {
     return (
         <div style={{position: 'fixed', top: 10, left: 10}}>
             {/* <img src={logoNutechNoTrans} alt="" width={350} /> */}
-            <img src={logoNutech} alt="" />
+            <img src={logoNutech} alt="" style={{width: '80%'}} />
         </div>
     )
 }
