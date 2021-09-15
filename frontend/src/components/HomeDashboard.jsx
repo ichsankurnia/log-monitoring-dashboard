55d57cc5d03b3f47895d9440f60758bcaa6ee21b
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-09-15 16:25:40 +0700

    add call center logo to home

--- frontend/src/components/HomeDashboard.jsx
+++ frontend/src/components/HomeDashboard.jsx
@@ -1,10 +1,16 @@
 import logoNutech from '../assets/img/logo-nutech.png'
+import logoCallCenter from '../assets/img/ccit-logo-trans.png'
 
 const HomeDashboard = () => {
     return (
-        <div style={{position: 'fixed', top: 10, left: 10}}>
-            {/* <img src={logoNutechNoTrans} alt="" width={350} /> */}
-            <img src={logoNutech} alt="" style={{width: '80%'}} />
+        <div>
+            <div style={{position: 'fixed', top: 10, left: 10}}>
+                {/* <img src={logoNutechNoTrans} alt="" width={350} /> */}
+                <img src={logoNutech} alt="" style={{width: '80%'}} />
+            </div>
+            <div style={{position: 'absolute', top: -90, right: -185}}>
+                <img src={logoCallCenter} alt="" style={{width: '60%'}} />
+            </div>
         </div>
     )
 }
